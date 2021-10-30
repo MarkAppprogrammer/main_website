ca711b478e532345ab8690348ddca72a4d399b88
MarkAppprogrammer <magib07@icloud.com>
2021-10-30 11:25:26 -0700

    did some secret stuff can you figure it out

---
+++ main.js
@@ -0,0 +1,32 @@
+let titleClicked = false
+let homeClicked = false
+let mainTitle = document.getElementById("main")
+let loginLink = document.getElementById("login")
+
+
+function setHomeClicked() {
+    homeClicked = true
+    showLogin()
+}
+
+
+
+function setValTitle() {
+    titleClicked = true
+    showLogin()
+}
+
+function showLogin() {
+    if (titleClicked == true) {
+        mainTitle.addEventListener("copy", setHomeClicked)
+
+        if (homeClicked == true) {
+            console.log("all clicked up")
+            console.log(document.getElementById("login").style.display)
+            document.getElementById(login).style.display = 'block';
+        }
+    }
+    else {
+        console.log("")
+    }
+}
